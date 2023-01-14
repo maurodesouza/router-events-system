130afc76e4e52dca0df6dab8cd584f510af52f9d
Mauro de Souza <maurodesouza2017@hotmail.com>
2023-01-14 19:44:33 -0300

    ⚡  chore: refactor queries page

--- src/pages/queries/index.tsx
+++ src/pages/queries/index.tsx
@@ -1,5 +1,12 @@
+import { Breadcrumb, Page, QueryForm } from "components"
+import { Database } from "phosphor-react"
+
 export const QueriesPage = () => {
   return (
-    <h1>Queries Page</h1>
+    <Page className="gap-10">
+      <Breadcrumb icon={Database} label="URL Queries" />
+
+      <QueryForm />
+    </Page>
   )
 }
