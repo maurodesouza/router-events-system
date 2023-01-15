bde7507022148ffb79ec70b05075d1603933d3f5
Mauro de Souza <maurodesouza2017@hotmail.com>
2023-01-14 22:31:07 -0300

    ⚡  chore: change default border radius value

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        DEFAULT: '0.5rem'
+      }
+    },
   },
   plugins: [],
 }
